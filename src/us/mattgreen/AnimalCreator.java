e5737fe72903322f23acea25f0c8ae4bb4530992
Jake Ferguson <jferguson15@my.wctc.edu>
2026-03-29 07:48:57 -0500

    build: Class fields and Constructor

--- src/us/mattgreen/AnimalCreator.java
+++ src/us/mattgreen/AnimalCreator.java
@@ -1,4 +1,20 @@
 package us.mattgreen;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AnimalCreator {
+    /*
+        - private for controlled access
+        - final = reference cant change
+        - reuse one scanner, don't recreate
+    */
+
+    private final  ArrayList<Talkable> zoo;
+    private final Scanner scanner;
+
+    public AnimalCreator(ArrayList<Talkable> zoo) {
+        this.zoo = zoo;
+        this.scanner = new Scanner(System.in);
+    }
 }
